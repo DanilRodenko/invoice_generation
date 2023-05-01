afe633fb74a6f69515b212eda4506aca5d73fd92
DR <94643493+DanilRodenko@users.noreply.github.com>
2023-05-01 18:24:31 +0100

    Add header and total

--- main.py
+++ main.py
@@ -6,8 +6,6 @@ from pathlib import Path
 filepaths = glob.glob('invoices/*.xlsx')
 
 for filepath in filepaths:
-    df = pd.read_excel(filepath, sheet_name="Sheet 1")
-
     pdf = FPDF(orientation='P', unit='mm', format='A4')
     pdf.add_page()
 
@@ -17,7 +15,45 @@ for filepath in filepaths:
     pdf.set_font('Times', size=16, style='B')
     pdf.cell(w=50, h=8, txt=f"Invoice nr. {invoice_nr}", ln=1)
     pdf.set_font('Times', size=16, style='B')
-    pdf.cell(w=50, h=8, txt=f"Date. {date}", ln=2)
+    pdf.cell(w=50, h=8, txt=f"Date. {date}", ln=1)
+
+    df = pd.read_excel(filepath, sheet_name="Sheet 1")
+
+    columns = list(df.columns)
+    pdf.set_font(family="Times", size=10)
+    pdf.set_text_color(80, 80, 80)
+    pdf.set_font(family='Times', style='B', size=10)
+    pdf.cell(w=30, h=8, txt=columns[0], border=1)
+    pdf.cell(w=60, h=8, txt=columns[1], border=1)
+    pdf.cell(w=40, h=8, txt=columns[2], border=1)
+    pdf.cell(w=30, h=8, txt=columns[3], border=1)
+    pdf.cell(w=30, h=8, txt=columns[4], border=1, ln=1)
+
+
+    for index, row in df.iterrows():
+        pdf.set_font(family="Times", size=10)
+        pdf.set_text_color(80, 80, 80)
+        pdf.cell(w=30, h=8, txt=str(row['product_id']), border=1)
+        pdf.cell(w=60, h=8, txt=str(row['product_name']), border=1)
+        pdf.cell(w=40, h=8, txt=str(row['amount_purchased']), border=1)
+        pdf.cell(w=30, h=8, txt=str(row['price_per_unit']), border=1)
+        pdf.cell(w=30, h=8, txt=str(row['total_price']), border=1, ln=1)
+
+    total_price = df['total_price'].sum()
+    pdf.set_font(family="Times", size=10)
+    pdf.set_text_color(80, 80, 80)
+    pdf.cell(w=30, h=8, txt="")
+    pdf.cell(w=60, h=8, txt="")
+    pdf.cell(w=40, h=8, txt="")
+    pdf.cell(w=30, h=8, txt="")
+    pdf.cell(w=30, h=8, txt=str(total_price), border=1, ln=1)
+
+    pdf.set_font(family="Times", size=10)
+    pdf.cell(w=30, h=8, txt=f"The total price is {total_price}", ln=1)
+
+    pdf.set_font(family="Times", size=10)
+    pdf.cell(w=30, h=8, txt=f"PythonHow")
+    pdf.image("pythonhow.png", w=10)
 
     pdf.output(f"PDFs/{filename}.pdf")
 
